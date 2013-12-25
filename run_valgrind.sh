358e3485c6d9fe69a987c64d8094ca0d1eb9ac51
Preetam Jinka <pj@isomero.us>
2013-12-25 15:04:49 -0500

    Print valgrind output if there's an error

--- run_valgrind.sh
+++ run_valgrind.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-okay=$(valgrind --tool=memcheck --leak-check=full --undef-value-errors=no ./test 2>&1 |  grep -c "in use at exit: 0 bytes in 0 blocks")
+valgrind_out=$(valgrind --tool=memcheck --leak-check=full --undef-value-errors=no ./test 2>&1)
+okay=$(echo $valgrind_out |  grep -c "in use at exit: 0 bytes in 0 blocks")
 
 if [ "$okay" = "1" ]; then
 	return 0
 fi
-echo "Memory leaks found!"
+echo "$valgrind_out"
 return 1
